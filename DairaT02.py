8aafaa1d3a786fe123dec1ded89f7abd43c82276
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-17 10:37:12 -0500

    Agregue siguientes propiedades

--- DairaT02.py
+++ DairaT02.py
@@ -7,6 +7,23 @@ class Mouse:
     @property
     def marca(self):
         return self.__marca
+    
     @marca.setter
     def marca(self,otraMarca):
         self.__marca = otraMarca
+
+     @property
+    def color(self):
+        return self.__color
+    
+    @color.setter
+    def color(self,nuevocolor):
+        self.__color = nuevocolor
+    
+     @property
+    def tipo(self):
+        return self.__tipo
+    
+    @tipo.setter
+    def tipo(self,otroTipo):
+        self.__tipo = otroTipo
