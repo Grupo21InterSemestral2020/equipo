6c25c05f130e460f64f874aa0d79551761cd9c0a
Paola Hinojosa Lozano <paola.honojosalz@gmail.com>
2020-07-21 16:08:09 -0500

    carpeta PIA

--- PIAequipo/cursos.py
+++ PIAequipo/cursos.py
@@ -1,5 +1,38 @@
 class Cursos:
-    def __init__(self,nombre,IdEmpleado,direccion):
-        self.__nombre = nombre
-        self.__IdEmpleado = IdEmpleado  
-        self.__direccion = direccion
+    def __init__(self,IdCurso,administracion,usuario):
+        self.__IdCurso = IdCurso
+        self.__administracion = administracion
+        self.__usuario = usuario
+
+    @property
+    def IdCurso(self):
+        return self.__IdCurso
+
+    @IdCurso.setter
+    def IdCurso(self,valor):
+        self.__IdCurso = valor
+
+    @property
+    def administracion(self):
+        return self.__administracion
+
+    @administracion.sette
+    def administracion(self,valor):
+        self.__administracion = valor
+
+    @property
+    def usuario(self):
+        return self.__usuario
+
+    @usuario.setter
+    def usuario(self,valor):
+        self.__usuario = valor
+
+    def imprimirInfo(self):
+        print(f"Id: {self.__IdCurso}")
+        print(f"administracion: {self.__administracion}")
+        print(f"usuario: {self.__usuario}")
+        
+
+
+    
