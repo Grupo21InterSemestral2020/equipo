952b57a8423cab0f70d28d9926b53bd2970d086d
Angie Sarahi Camarillo Ramirez <angieramirez0111@gmail.com>
2020-07-21 15:54:50 -0500

    init clase empleado

--- PIAequipo/empleados.py
+++ PIAequipo/empleados.py
@@ -1,2 +1,7 @@
 class Empleado():
-    pass
+    def __init__(self,idEmpleado,nombre,direccion):
+        self.__idEmpleado= idEmpleado
+        self.__nombre = nombre
+        self.__direccion = direccion
+
+    
