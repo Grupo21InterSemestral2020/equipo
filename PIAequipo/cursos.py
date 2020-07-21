0fe31283ef4d3332cd86d55c99bf5a72589fe872
Paola Hinojosa Lozano <paola.honojosalz@gmail.com>
2020-07-21 15:47:18 -0500

    carpeta PIA

--- PIAequipo/cursos.py
+++ PIAequipo/cursos.py
@@ -1,5 +1,5 @@
 class Cursos:
     def __init__(self,nombre,IdEmpleado,direccion):
         self.__nombre = nombre
-        self.__Idempleado = IdEmpleado  
+        self.__IdEmpleado = IdEmpleado  
         self.__direccion = direccion
