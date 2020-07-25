96ba41951dc219db02c1cd377f31023f41010fac
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-25 15:20:09 -0500

    elimine archivo duplicado

--- PIAequipo/menu.py
+++
@@ -1,6 +0,0 @@
-from cursos import Cursos
-from curso_temas import Curso_Tema
-from curso_temas_videos import Curso_tema_video
-from empleados import Empleado
-from temas import Tema
-from videos import Video
