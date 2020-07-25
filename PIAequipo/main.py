ca18ce7ac8c63ceb346bb139dc27007b64e6dd7c
Maria Fatima Rubi Gzz Gzz <fatima2000g3@gmail.com>
2020-07-25 16:55:22 -0500

    menu primera parte

--- PIAequipo/main.py
+++ PIAequipo/main.py
@@ -10,10 +10,43 @@ while True:
             opcion=int(input('''
             ¿Donde desea interactuar?
             1)Empleados
-            2)Curso
+            2)Cursos
             3)Temas
             4)Videos
-            5)Temas Asignados
+            5)Temas_Curso
             6)VideosAsignados
             7)Salir
             >>Seleccione opcion: '''))
+            
+            if opcion == 1:
+                valor=Empleado(0,None,None)
+                valor.menu()
+
+            elif opcion == 2:
+                valor=Cursos(0,None,None)
+                valor.minimenu()  #corregir nombre
+
+            elif opcion == 3:
+                valor=Tema(0,None,None)
+                valor.opciones()  #corregir nombre
+
+            elif opcion == 4:
+                valor=Video(0,None,None,None)
+                valor.minimenu() #corregir nombre
+
+            elif opcion == 5:
+                valor=Curso_Tema(0,None,None)
+                valor.menu() 
+
+            elif opcion == 6:
+                valor=Curso_tema_video(0,None,None)
+                valor.minimenu()    #corregir nombre y hacer menu     
+
+            elif opcion == 7:
+                break
+
+        except :
+            #limpiar_pantalla()
+            input("Error, introducir unicamente numero, vuelva a intentar, enter para continuar...")
+
+        
