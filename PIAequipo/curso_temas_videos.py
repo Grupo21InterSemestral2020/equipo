95a65022935948e87f35565d68a9524a18d92b00
Paola Hinojosa Lozano <paola.honojosalz@gmail.com>
2020-07-25 14:17:36 -0500

    agregue property

--- PIAequipo/curso_temas_videos.py
+++ PIAequipo/curso_temas_videos.py
@@ -4,4 +4,26 @@ class Cuso_tema_video:
         self.__IdCT = IdCT
         self.__IdVideo = IdVideo
 
-        
+    @property
+    def IdCursoTV(self):
+        return self.__IdCursoTV
+
+    @IdCursoTV.setter
+    def IdCursoTV(self,valor):
+        self.__IdCursoTV = valor
+
+    @property
+    def IdCT(self):
+        return self.__IdCT
+
+    @IdCT.setter
+    def IdCT(self,valor):
+        self.__IdCT = valor
+
+    @property
+    def IdVideo(self):
+        return self.__IdVideo
+
+    @IdVideo.setter
+    def IdVideo(self,valor):
+        self.__IdVideo = valor 
