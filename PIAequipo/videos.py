90c0da20b26fd91e3a82deb89ecd1ef017f8fc1f
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-25 14:54:31 -0500

    agregue opciones a clase video

--- PIAequipo/videos.py
+++ PIAequipo/videos.py
@@ -29,26 +29,79 @@ class Video:
     def fechap(self,valor):
         self.__fechap = valor
 
-        def Minimenu(self):
-            lista=[]
-            while True:
-                try:
-                    opcion=int(input('''
-                    Elige una opcion
-                    1)Agregarvideo
-                    2)Eliminarvideo
-                    3)Modificarvideo
-                    4)Vervideos
-                    5)Elegir Id
-                    6)Salir
-                    >>Opcion:"))'''
+    def __eq__ (self,otro):
+        return self.__idV==otro.__idV
 
-                    if opcion==1:
-                        self.__idV=self.__idV+1
-                        self.__nom= input("Asigna el nombre del video: ")
-                        self.__url=int(input("Ingresa la url del video: "))
-                        self.__fechap=int(input("Ingresa la fecha del video: "))
-                    
-                        datos=Video(self.__idV,self.__nom,self.__url,self.__fechap)
-                        lista.append(datos)
-                        print("Se ha agregado el registro")
+    def Minimenu(self):
+        lista=[]
+        while True:
+            try:
+                opcion=int(input('''
+                Elige una opcion
+                1)Agregarvideo
+                2)Eliminarvideo
+                3)Modificarvideo
+                4)Vervideos
+                5)Elegir Id
+                6)Salir
+                >>Opcion: '''))
+
+                if opcion==1:
+                    self.__idV=self.__idV+1
+                    self.__nom= input("Asigna el nombre del video: ")
+                    self.__url=int(input("Ingresa la url del video: "))
+                    self.__fechap=int(input("Ingresa la fecha del video: "))
+
+                    datos=Video(self.__idV,self.__nom,self.__url,self.__fechap)
+                    lista.append(datos)
+                    print("Se ha agregado el registro")
+
+                elif opcion==2:
+                    print(f"\n{'idVideo':<30}{'nombre':<30}{'url':<30}{'fecha':<30}")
+                    for i1 in lista:
+                        print(f"{i1.idV:<30}{i1.nom:<30}{i1.url:<30}{i1.fechap:<30}")
+                    if lista==[]:
+                        input("Esta vacia, enter para continuar...")
+                    else:
+                        clave=int(input("Introduce clave:"))
+                        for remover in lista:
+                            if remover.idV == clave:
+                                lista.remove(Video(clave,None,None,None))
+                            input("Registro eliminado, enter para continuar...")
+
+                elif opcion==3:
+                    print(f"\n{'idVideo':<30}{'nombre':<30}{'url':<30}{'fecha':<30}")
+                    for i2 in lista:
+                        print(f"{i2.idV:<30}{i2.nom:<30}{i2.url:<30}{i2.fechap:<30}")
+                    clave=int(input("Clave:"))
+                    if lista==[]:
+                        input("Este registro esta vacio actualmente, enter para continuar...")
+                    else:
+                        for remover in lista:
+                            if remover.idV==clave:
+                                remover.nom=("Ingresa un nombre nuevo: ")
+                                remover.url=int(input("Ingresa una url nueva: "))
+                                remover.fechap=int(input("Ingresa una fecha nueva: "))
+                            input("El registro actualizado, enter para continuar...")
+ 
+                elif opcion==4:
+                    if lista==[]:
+                        input("Registro vacio actualmente, enter para continuar...")
+                    else:
+                        print(f"\n{'idVideo':<30}{'nombre':<30}{'url':<30}{'fecha':<30}")
+                        for i3 in lista:
+                            print(f"{i3.idV:<30}{i3.nom:<30}{i3.url:<30}{i3.fechap:<30}")
+
+                elif opcion==5:
+                    print(f"\n{'idVideo':<30}{'nombre':<30}{'url':<30}{'fecha':<30}")
+                    for i4 in lista:
+                        print(f"{i4.idV:<30}{i4.nom:<30}{i4.url:<30}{i4.fechap:<30}")
+                        clave=int(input("Clave:"))
+                        print(f"\n{'idVideo':<30}{'nombre':<30}{'url':<30}{'fecha':<30}")
+                    for i5 in lista:
+                        if i5.icttv==clave:
+                            print(f"{i5.idV:<30}{i5.nom:<30}{i5.url:<30}{i5.fechap:<30}")
+                elif opcion==6:
+                    break
+                elif opcion>6:
+                    input("Introduce un numero valido")
