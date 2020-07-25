817782b5be64e000b26054e10cd3a45994237032
Paola Hinojosa Lozano <paola.honojosalz@gmail.com>
2020-07-25 15:00:30 -0500

    agregue import

---
+++ PIAequipo/menu.py
@@ -0,0 +1,6 @@
+from cursos import Cursos
+from curso_temas import Curso_Tema
+from curso_temas_videos import Curso_tema_video
+from empleados import Empleado
+from temas import Tema
+from videos import Video
