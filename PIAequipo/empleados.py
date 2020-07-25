8b937b091c3b04fd65ede42255eeb215183b980e
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-25 11:28:39 -0500

    agregue opciones

--- PIAequipo/empleados.py
+++ PIAequipo/empleados.py
@@ -29,48 +29,6 @@ class Empleado():
         self.__direccion = direccion
     
 
-
-    def modificarEmpleado():
-        print("Modificacion de parametros: ")
-        archivo = open("./PIAequipo/empleados.txt","r",encoding="utf8")
-        lines = archivo.readlines()
-        archivo.close()
-        archivo = open("./PIAequipo/empleados.txt","w",encoding="utf8")
-        idEmpleado = input("Ingrese el Id del empleado que desea modificar: ")
-
-        for line in lines:
-            id = line.split("|")[0]
-            if idEmpleado != id:
-                archivo.write(line)
-
-            else:
-                print("Ingrese nuevos datos del empleado: ")
-                idEmpleado = input("Ingrese id: ")
-                nombre = str(input("Ingrese nombre: "))
-                direccion = str(input("Ingrese su direccion: "))
-                archivo.writelines(idEmpleado + "|" + nombre + "|" + direccion + "|" + "\n" )
-
-        archivo.close()      
-        
-    def consultarEmpleado(self):
-        #print("Ingresa el Id del empleado que desea consultar: ")
-        archivo = open("../PIAequipo/empleados.txt","r",encoding="utf8")
-        lines = archivo.readlines()
-        for line in lines:
-                print (line)
-        archivo.close()
-    
-    def verDetalles(self):
-        archivo = open("../PIAequipo/empleados.txt","r",encoding="utf8")  
-        id_detalles = input("Ingrese id del empleado: ")
-        lines = archivo.readlines()
-
-        for line in lines:
-            id = line.split('|')[0]
-            if id_detalles == id:
-                print (line)
-        archivo.close()
-        
     def menu(self):
 
         empleados = []
@@ -85,12 +43,13 @@ class Empleado():
 
             opcion= int(input("Elija una opcion: "))
             if opcion ==1:
-                f = open("./PIAequipo/empleados.txt","a",encoding="utf8")
+                f = open("./archivos/empleados.txt","a",encoding="utf8")
                 self.__idEmpleado= self.__idEmpleado+1
                 self.__nombre=input("Ingresa el nombre del empleado: ")
                 self.__direccion=input("Ingresa la direccion del empleado")
                 f.write(self.__idEmpleado,self.__nombre,self.__direccion)
-       
+                f.closed()
+
             elif opcion ==2:
                 if empleados==[]:
                     input("Actualmente vacia")
@@ -102,7 +61,7 @@ class Empleado():
                             input("Registro borrado")
             
             elif opcion ==3:
-                clave = int(input("clave: "))
+                clave = int(input("Introduce la clave: "))
                 if empleados ==[]:
                     input("Actualmente vacia")
                 
@@ -113,6 +72,20 @@ class Empleado():
                             remover.direccion = input("Ingrese nueva direccion: ")
                             input("Regirstro actualizado correctamente...")
 
+            elif opcion==4:
+                if empleados==[]:
+                    input("Registro vacio actualmente")
+                else:
+                    print(f"{'idEmpleado':<20}{'nombre':<20}{'direccion':<20}")
+
+                    for posicion in empleados:
+                        print(f"{posicion.idEmpleado:<20}{posicion.nombre:<20}{posicion.direccion:<20}")
+            
+            elif opcion == 5:
+                break 
+            
+            elif opcion>5:
+                input("Error, introduzca numero del minimenu")
 
 
             def guardado():
@@ -120,4 +93,4 @@ class Empleado():
                 for posicion in empleados:
                     archivo.write(str(f" idEmpleado: {posicion.idEmpleado}, nombre: {posicion.nombre}, direccion: {posicion.direccion} ""\n"))
                 archivo.close()
-            guardado()
+            guardado()       
