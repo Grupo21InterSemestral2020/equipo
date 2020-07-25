2ff8ba4153f68c2e83479a0e12ec89169fe49cf2
Daira Yllen Torres Gonzalez <d41r4tg@gmail.com>
2020-07-25 12:57:47 -0500

    agregue input en main

--- PIAequipo/main.py
+++ PIAequipo/main.py
@@ -2,3 +2,17 @@ from cursos import Cursos
 from empleados import Empleado
 from temas import Tema
 from curso_temas import Curso_Tema
+
+while True:
+        try:
+            opcion=int(input('''
+            ¿Donde desea interactuar?
+            1)Empleados
+            2)Curso
+            3)Temas
+            4)Videos
+            5)Temas Asignados
+            6)VideosAsignados
+            7)Salir
+            >>Seleccione opcion: "))
+            '''
